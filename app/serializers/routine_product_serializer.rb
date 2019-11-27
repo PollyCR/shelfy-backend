126fa78fad7dd741a77cccf5359424846847ce8b
PollyCR <polly.c.reynolds@gmail.com>
2019-11-27 14:11:02 +0000

    removed coments

--- app/serializers/routine_product_serializer.rb
+++ app/serializers/routine_product_serializer.rb
@@ -1,6 +1,5 @@
 class RoutineProductSerializer < ActiveModel::Serializer
   attributes :id, :product_id, :routine_id, :active_ingredients
   belongs_to :product
-  has_many :comments
   has_many :active_ingredients, through: :products
 end
