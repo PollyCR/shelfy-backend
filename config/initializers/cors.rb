b9f5207a1c4664421ea468fa1015a1d0cc705a94
PollyCR <polly.c.reynolds@gmail.com>
2020-01-16 11:51:28 +0000

    cors erroring out still

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /https:\/\/shelfy.netlify.com\/*/
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
